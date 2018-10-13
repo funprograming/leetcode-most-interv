6146957c2cb9ba2f9fb83af22e8206b9512ac034
fwp <funprograming@hotmail.com>
2018-10-13 17:30:21 +0800

    add l.138

--- leetcode138_Copy List with Random Pointer.cpp
+++ leetcode138_Copy List with Random Pointer.cpp
@@ -13,7 +13,7 @@ Return a deep copy of the list.
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
-// way 1 recursive
+// way 1 recursive Space o(n) Time O(n)
 class Solution {
 private:
     unordered_map<RandomListNode *,RandomListNode *> nodemap;
@@ -30,5 +30,41 @@ public:
         copyhead->random = copyRandomList(head->random);
 
         return copyhead;
+    }
+};
+
+// way 2 Space o(1) Time O(n)
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        // copy new next and insert after old ones
+        RandomListNode *head1 = head;
+        while(nullptr!=head1){
+            RandomListNode *tmp = new RandomListNode(head1->label);
+            tmp->next = head1->next;
+            head1->next = tmp;
+            head1 = tmp->next;
+        }
+        // copy new random and connect
+        head1 = head;
+        while(nullptr!=head1){
+            //head1->next will never be null,so head1->next->random is ok to use
+            head1->next->random = head1->random ? head1->random->next:nullptr;
+            head1 = head1->next->next;
+        }
+        // serparate two linked list
+        RandomListNode *head_old = head;
+        RandomListNode *head_new = head->next;
+        RandomListNode *head_rst = head->next;
+        while(nullptr!=head_old){
+            head_old->next =   head_old->next->next;
+            head_new->next = head_new->next ? head_new->next->next:nullptr;
+            head_old = head_old->next;
+            head_new = head_new->next;
+        }
+        return head_rst;
     }
 };
