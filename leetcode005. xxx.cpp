363bd305a125b9277a05e07cd6547e4902d378ab
fwp <funprograming@hotmail.com>
2019-06-19 23:44:43 +0800

    modif

--- leetcode005. xxx.cpp
+++ leetcode005. xxx.cpp
@@ -1,57 +1,19 @@
-/* leetcode3. Longest Substring Without Repeating Characters
-Given a string, find the length of the longest substring without repeating characters.
+/* leetcode5. Longest Palindromic Substring
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
 Example 1:
 
-Input: "abcabcbb"
-Output: 3 
-Explanation: The answer is "abc", with the length of 3. 
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
 Example 2:
 
-Input: "bbbbb"
-Output: 1
-Explanation: The answer is "b", with the length of 1.
-Example 3:
-
-Input: "pwwkew"
-Output: 3
-Explanation: The answer is "wke", with the length of 3. 
-             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Input: "cbbd"
+Output: "bb"
 */
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        if(!n) return 0;
-		int maxlen = 1;
-		for(int i = n-1;i>=0;--i){
-			maxlen = max(maxlen,helper(s,i));
-		}
-		return maxlen;
-    }
-	int helper(string s,int i){
-		set<char> strs;
-		while(i<s.size() && (strs.end() == strs.find(s[i]))){
-			strs.insert(s[i]);
-			++i;
-		}
-		return strs.size();
-	}
-};
-
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> v(256,-1);
+    string longestPalindrome(string s) {
         
-        int i,start=-1,ans=0;
-        for(i=0;i<s.size();i++){
-            if(v[s[i]]>start){
-                start = v[s[i]];
-            }
-            v[s[i]] = i;
-            ans = max(ans,i-start);
-        }
-        return ans;
     }
 };
